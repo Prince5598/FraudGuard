1b2591a1f517dd17a633083e89453ea562bdecfb
prince <princepatel5598@gmail.com>
2025-06-12 12:42:55 +0530

    Removed folders from main branch

--- backend/config/Db.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
-
-exports.connect = () => {
-    mongoose.connect(process.env.MONGODB_URL);
-} 
--- backend/routes/adminProtected.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const authMiddleware = require('../middleware/auth');
-const role = require('../middleware/role');
-
-router.get("/panel", authMiddleware, (req, res) => {
-  res.json({ message: "Welcome to the Admin Panel", admin: req.user });
-});
-
-module.exports = router;
